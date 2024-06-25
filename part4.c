8335a215c3bd8e769224affc80ccace09e12ec1f
yuvalbarak8 <155367840+yuvalbarak8@users.noreply.github.com>
2024-06-25 15:12:41 +0300

    Create part4.c

---
+++ part4.c
@@ -0,0 +1,43 @@
+// main.c
+#include "copytree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void print_usage(const char *prog_name) {
+    fprintf(stderr, "Usage: %s [-l] [-p] <source_directory> <destination_directory>\n", prog_name);
+    fprintf(stderr, "  -l: Copy symbolic links as links\n");
+    fprintf(stderr, "  -p: Copy file permissions\n");
+}
+
+int main(int argc, char *argv[]) {
+    int opt;
+    int copy_symlinks = 0;
+    int copy_permissions = 0;
+
+    while ((opt = getopt(argc, argv, "lp")) != -1) {
+        switch (opt) {
+            case 'l':
+                copy_symlinks = 1;
+                break;
+            case 'p':
+                copy_permissions = 1;
+                break;
+            default:
+                print_usage(argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (optind + 2 != argc) {
+        print_usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *src_dir = argv[optind];
+    const char *dest_dir = argv[optind + 1];
+
+    copy_directory(src_dir, dest_dir, copy_symlinks, copy_permissions);
+
+    return 0;
+}
