6bea9628f11293201b8c4d84a00eae8eba2d47b5
yuvalbarak8 <155367840+yuvalbarak8@users.noreply.github.com>
2024-06-26 16:32:26 +0300

    Update buffered_open.c

--- buffered_open.c
+++ buffered_open.c
@@ -8,165 +8,165 @@
 
 buffered_file_t *buffered_open(const char *pathname, int flags, ...) 
 {
-    buffered_file_t *bf = (buffered_file_t *)malloc(sizeof(buffered_file_t));
-    if (!bf) {
+    buffered_file_t *bfile = (buffered_file_t *)malloc(sizeof(buffered_file_t));
+    if (!bfile) {
         return NULL;
     }
 
-    va_list args;
-    va_start(args, flags);
-    mode_t mode = 0;
+    va_list arg_list;
+    va_start(arg_list, flags);
+    mode_t file_mode = 0;
     if (flags & O_CREAT) {
-        mode = va_arg(args, int);
+        file_mode = va_arg(arg_list, int);
     }
-    va_end(args);
+    va_end(arg_list);
 
-    bf->preappend = (flags & O_PREAPPEND) ? 1 : 0;
+    bfile->preappend = (flags & O_PREAPPEND) ? 1 : 0;
     flags &= ~O_PREAPPEND;
 
-    bf->fd = open(pathname, flags, mode);
-    if (bf->fd == -1) {
-        free(bf);
+    bfile->fd = open(pathname, flags, file_mode);
+    if (bfile->fd == -1) {
+        free(bfile);
         return NULL;
     }
 
-    bf->read_buffer = (char *)malloc(BUFFER_SIZE);
-    bf->write_buffer = (char *)malloc(BUFFER_SIZE);
-    if (!bf->read_buffer || !bf->write_buffer) {
-        close(bf->fd);
-        free(bf->read_buffer);
-        free(bf->write_buffer);
-        free(bf);
+    bfile->read_buffer = (char *)malloc(BUFFER_SIZE);
+    bfile->write_buffer = (char *)malloc(BUFFER_SIZE);
+    if (!bfile->read_buffer || !bfile->write_buffer) {
+        close(bfile->fd);
+        free(bfile->read_buffer);
+        free(bfile->write_buffer);
+        free(bfile);
         return NULL;
     }
 
-    bf->read_buffer_size = 0;
-    bf->write_buffer_size = 0;
-    bf->read_buffer_pos = 0;
-    bf->write_buffer_pos = 0;
-    bf->flags = flags;
+    bfile->read_buffer_size = 0;
+    bfile->write_buffer_size = 0;
+    bfile->read_buffer_pos = 0;
+    bfile->write_buffer_pos = 0;
+    bfile->flags = flags;
 
-    return bf;
+    return bfile;
 }
 
-ssize_t buffered_write(buffered_file_t *bf, const void *buf, size_t count) 
+ssize_t buffered_write(buffered_file_t *bfile, const void *buf, size_t count) 
 {
-    if (bf->preappend) {
-        off_t current_offset = lseek(bf->fd, 0, SEEK_END);
-        char *temp_buffer = (char *)malloc(current_offset);
-        if (!temp_buffer) {
+    if (bfile->preappend) {
+        off_t end_offset = lseek(bfile->fd, 0, SEEK_END);
+        char *temp_buf = (char *)malloc(end_offset);
+        if (!temp_buf) {
             errno = ENOMEM;
             return -1;
         }
 
-        lseek(bf->fd, 0, SEEK_SET);
-        if (read(bf->fd, temp_buffer, current_offset) == -1) {
-            free(temp_buffer);
+        lseek(bfile->fd, 0, SEEK_SET);
+        if (read(bfile->fd, temp_buf, end_offset) == -1) {
+            free(temp_buf);
             return -1;
         }
 
-        lseek(bf->fd, 0, SEEK_SET);
-        if (write(bf->fd, buf, count) == -1) {
-            free(temp_buffer);
+        lseek(bfile->fd, 0, SEEK_SET);
+        if (write(bfile->fd, buf, count) == -1) {
+            free(temp_buf);
             return -1;
         }
 
-        if (write(bf->fd, temp_buffer, current_offset) == -1) {
-            free(temp_buffer);
+        if (write(bfile->fd, temp_buf, end_offset) == -1) {
+            free(temp_buf);
             return -1;
         }
 
-        free(temp_buffer);
+        free(temp_buf);
         return count;
     }
 
-    const char *buffer = (const char *)buf;
-    size_t remaining = count;
+    const char *input_buf = (const char *)buf;
+    size_t bytes_left = count;
 
-    while (remaining > 0) {
-        if (bf->write_buffer_pos > 0) {
-            ssize_t written = write(bf->fd, bf->write_buffer, bf->write_buffer_pos);
-            if (written == -1) {
+    while (bytes_left > 0) {
+        if (bfile->write_buffer_pos > 0) {
+            ssize_t written_bytes = write(bfile->fd, bfile->write_buffer, bfile->write_buffer_pos);
+            if (written_bytes == -1) {
                 return -1;
             }
-            bf->write_buffer_pos = 0;
+            bfile->write_buffer_pos = 0;
         }
 
-        size_t space = BUFFER_SIZE - bf->write_buffer_pos;
-        size_t to_write = (remaining < space) ? remaining : space;
+        size_t buffer_space = BUFFER_SIZE - bfile->write_buffer_pos;
+        size_t write_bytes = (bytes_left < buffer_space) ? bytes_left : buffer_space;
 
-        memcpy(bf->write_buffer + bf->write_buffer_pos, buffer, to_write);
-        bf->write_buffer_pos += to_write;
-        buffer += to_write;
-        remaining -= to_write;
+        memcpy(bfile->write_buffer + bfile->write_buffer_pos, input_buf, write_bytes);
+        bfile->write_buffer_pos += write_bytes;
+        input_buf += write_bytes;
+        bytes_left -= write_bytes;
     }
 
     return count;
 }
 
-ssize_t buffered_read(buffered_file_t *bf, void *buf, size_t count) 
+ssize_t buffered_read(buffered_file_t *bfile, void *buf, size_t count) 
 {
-    char *buffer = (char *)buf;
-    size_t remaining = count;
+    char *output_buf = (char *)buf;
+    size_t bytes_left = count;
 
-    while (remaining > 0) {
-        if (bf->read_buffer_pos == bf->read_buffer_size) {
-            bf->read_buffer_size = read(bf->fd, bf->read_buffer, BUFFER_SIZE);
-            if (bf->read_buffer_size == -1) {
+    while (bytes_left > 0) {
+        if (bfile->read_buffer_pos == bfile->read_buffer_size) {
+            bfile->read_buffer_size = read(bfile->fd, bfile->read_buffer, BUFFER_SIZE);
+            if (bfile->read_buffer_size == -1) {
                 return -1;
             }
-            if (bf->read_buffer_size == 0) {
+            if (bfile->read_buffer_size == 0) {
                 break;
             }
-            bf->read_buffer_pos = 0;
+            bfile->read_buffer_pos = 0;
         }
 
-        size_t to_read = bf->read_buffer_size - bf->read_buffer_pos;
-        if (to_read > remaining) {
-            to_read = remaining;
+        size_t read_bytes = bfile->read_buffer_size - bfile->read_buffer_pos;
+        if (read_bytes > bytes_left) {
+            read_bytes = bytes_left;
         }
 
-        memcpy(buffer, bf->read_buffer + bf->read_buffer_pos, to_read);
-        bf->read_buffer_pos += to_read;
-        buffer += to_read;
-        remaining -= to_read;
+        memcpy(output_buf, bfile->read_buffer + bfile->read_buffer_pos, read_bytes);
+        bfile->read_buffer_pos += read_bytes;
+        output_buf += read_bytes;
+        bytes_left -= read_bytes;
     }
 
-    return count - remaining;
+    return count - bytes_left;
 }
 
-int buffered_flush(buffered_file_t *bf) 
+int buffered_flush(buffered_file_t *bfile) 
 {
-    if (bf->write_buffer_pos > 0) {
-        ssize_t written = write(bf->fd, bf->write_buffer, bf->write_buffer_pos);
-        if (written == -1) {
+    if (bfile->write_buffer_pos > 0) {
+        ssize_t written_bytes = write(bfile->fd, bfile->write_buffer, bfile->write_buffer_pos);
+        if (written_bytes == -1) {
             return -1;
         }
-        bf->write_buffer_pos = 0;
+        bfile->write_buffer_pos = 0;
     }
 
     return 0;
 }
 
-int buffered_close(buffered_file_t *bf) 
+int buffered_close(buffered_file_t *bfile) 
 {
-    int result = 0;
+    int close_result = 0;
 
-    if (bf->write_buffer_pos > 0) {
-        ssize_t written = write(bf->fd, bf->write_buffer, bf->write_buffer_pos);
-        if (written == -1) {
-            result = -1;
+    if (bfile->write_buffer_pos > 0) {
+        ssize_t written_bytes = write(bfile->fd, bfile->write_buffer, bfile->write_buffer_pos);
+        if (written_bytes == -1) {
+            close_result = -1;
         }
-        bf->write_buffer_pos = 0;
+        bfile->write_buffer_pos = 0;
     }
 
-    if (close(bf->fd) == -1) {
-        result = -1;
+    if (close(bfile->fd) == -1) {
+        close_result = -1;
     }
 
-    free(bf->read_buffer);
-    free(bf->write_buffer);
-    free(bf);
+    free(bfile->read_buffer);
+    free(bfile->write_buffer);
+    free(bfile);
 
-    return result;
+    return close_result;
 }
