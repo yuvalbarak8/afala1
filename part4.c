0e6cc2165300edb2843db6f8662c8e2f7b4cfefc
yuvalbarak8 <155367840+yuvalbarak8@users.noreply.github.com>
2024-06-26 14:13:10 +0300

    Update part4.c

--- part4.c
+++ part4.c
@@ -1,50 +1,52 @@
-// main.c
-#include "copytree.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <unistd.h>  
+#include "copytree.h"  
 
-void print_usage(const char *prog_name) {
-    fprintf(stderr, "Usage: %s [-l] [-p] <source_directory> <destination_directory>\n", prog_name);
+void show_usage(const char *program_name)
+{
+    // Function to display usage information
+    fprintf(stderr, "Usage: %s [-l] [-p] <source_directory> <destination_directory>\n", program_name);
     fprintf(stderr, "  -l: Copy symbolic links as links\n");
     fprintf(stderr, "  -p: Copy file permissions\n");
 }
 
-int main(int argc, char *argv[]) {
-    int opt;
-    int copy_symlinks = 0;
-    int copy_permissions = 0;
+int main(int argc, char *argv[])
+{
+    int option;
+    int do_copy_symlinks = 0;
+    int do_copy_permissions = 0;
 
-    // Handle command-line flags
-    while ((opt = getopt(argc, argv, "lp")) != -1) {
-        switch (opt) {
+    // Parse command-line options
+    while ((option = getopt(argc, argv, "lp")) != -1)
+    {
+        switch (option)
+        {
             case 'l':
-                copy_symlinks = 1;
+                do_copy_symlinks = 1;  // Set flag to copy symbolic links
                 break;
             case 'p':
-                copy_permissions = 1;
+                do_copy_permissions = 1;  // Set flag to copy file permissions
                 break;
             default:
-                print_usage(argv[0]);
-                return EXIT_FAILURE;
+                show_usage(argv[0]);  // Display usage information for incorrect options
+                return EXIT_FAILURE;  // Exit with failure status
         }
     }
 
-    if (optind + 2 != argc) {
-        print_usage(argv[0]);
-        return EXIT_FAILURE;
+    // Check if correct number of arguments are provided
+    if (optind + 2 != argc)
+    {
+        show_usage(argv[0]);  // Display usage information if arguments are incorrect
+        return EXIT_FAILURE;  // Exit with failure status
     }
 
-    const char *src_dir = argv[optind];
-    const char *dest_dir = argv[optind + 1];
+    // Extract source and destination directories
+    const char *source_dir = argv[optind];
+    const char *destination_dir = argv[optind + 1];
 
-    // Debug messages to check the flags
-    printf("Source Directory: %s\n", src_dir);
-    printf("Destination Directory: %s\n", dest_dir);
-    printf("Copy Symbolic Links: %s\n", copy_symlinks ? "Yes" : "No");
-    printf("Copy File Permissions: %s\n", copy_permissions ? "Yes" : "No");
+    // Call function to copy directory
+    copy_directory(source_dir, destination_dir, do_copy_symlinks, do_copy_permissions);
 
-    copy_directory(src_dir, dest_dir, copy_symlinks, copy_permissions);
-
-    return 0;
+    return 0;  // Exit with success status
 }
